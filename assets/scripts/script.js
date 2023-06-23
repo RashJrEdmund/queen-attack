ef9a61703bd500dc76304d8800cb4d1619cae1d2
rash <orashusedmund@gmail.com>
2023-06-23 04:33:51 +0100

    fixing linters

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,55 +1,54 @@
-import { createChessBoard, isStrikeable } from "./utils.js"; // FUNCTION TO CREATE CHESS  BOARD
+import { createChessBoard, isStrikeable } from './utils.js' // FUNCTION TO CREATE CHESS  BOARD
 
-const chessBoard = document.getElementById("chess-board");
-const boardSize = 64;
+const chessBoard = document.getElementById('chess-board')
+const boardSize = 64
 let choosenQueens = [];
 
-(() => createChessBoard(boardSize, chessBoard))(); // ANONYMOUS FUNCTION THAT CALLS createChessBoard()
+(() => createChessBoard(boardSize, chessBoard))() // ANONYMOUS FUNCTION THAT CALLS createChessBoard()
 
-const squares = document.querySelectorAll(".board-square");
-const errorSuccessTag = document.getElementsByTagName("p")[0];
-const checkAttackBtn = document.getElementById("check-attack");
-const resetBtn = document.getElementById("reset-btn");
+const squares = document.querySelectorAll('.board-square')
+const errorSuccessTag = document.getElementsByTagName('p')[0]
+const checkAttackBtn = document.getElementById('check-attack')
+const resetBtn = document.getElementById('reset-btn')
 
 squares.forEach((square) =>
-  square.addEventListener("click", ({ target }) => {
-    const { id } = target;
-    errorSuccessTag.innerHTML = "";
-    errorSuccessTag.classList.remove("success");
+  square.addEventListener('click', ({ target }) => {
+    const { id } = target
+    errorSuccessTag.innerHTML = ''
+    errorSuccessTag.classList.remove('success')
 
-    if (choosenQueens.length >= 2)
-      return (errorSuccessTag.innerHTML = "cannot select more than 1 queen");
+    if (choosenQueens.length >= 2) { return (errorSuccessTag.innerHTML = 'cannot select more than 1 queen') }
 
     if (choosenQueens.length < 1) {
       // means this is our firts queen
-      target.classList.add("queen_1");
+      target.classList.add('queen_1')
     } else {
-      target.classList.add("queen_2");
+      target.classList.add('queen_2')
     }
 
-    choosenQueens.push(id);
+    choosenQueens.push(id)
   })
-);
+)
 
 const resetGame = () => {
   choosenQueens.forEach((id) => {
-    document.getElementById(id).classList.remove("queen_1");
-    document.getElementById(id).classList.remove("queen_2");
-  });
+    document.getElementById(id).classList.remove('queen_1')
+    document.getElementById(id).classList.remove('queen_2')
+  })
 
-  errorSuccessTag.innerHTML = "";
-  choosenQueens = [];
-};
+  errorSuccessTag.innerHTML = ''
+  choosenQueens = []
+}
 
-resetBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener('click', resetGame)
 
-checkAttackBtn.addEventListener("click", () => {
-  const attackObj = isStrikeable(choosenQueens, errorSuccessTag);
+checkAttackBtn.addEventListener('click', () => {
+  const attackObj = isStrikeable(choosenQueens, errorSuccessTag)
   if (attackObj.isValid) {
-    errorSuccessTag.classList.add("success");
-    errorSuccessTag.innerHTML = attackObj.message;
-    return;
+    errorSuccessTag.classList.add('success')
+    errorSuccessTag.innerHTML = attackObj.message
+    return
   }
 
-  errorSuccessTag.innerHTML = attackObj.message;
-});
+  errorSuccessTag.innerHTML = attackObj.message
+})
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,79 +1,80 @@
 export const createChessBoard = (size, chessBoard) => {
-  const rowBycol = Math.floor(Math.sqrt(size));
-  let idTracker = 11; // button id's should start from 11
+  const rowBycol = Math.floor(Math.sqrt(size))
+  let idTracker = 11 // button id's should start from 11
   const exceptions = [
     9, 10, 19, 20, 29, 30, 39, 40, 49, 50, 59, 60, 69, 70, 79, 80, 89, 90, 99,
-    100,
-  ];
+    100
+  ]
 
   for (let i = 0; i < rowBycol; i++) {
     for (let j = 0; j < rowBycol; j++) {
-      const boardSquare = document.createElement("button");
-      boardSquare.className = "board-square";
-      boardSquare.type = "button";
+      const boardSquare = document.createElement('button')
+      boardSquare.className = 'board-square'
+      boardSquare.type = 'button'
 
-      chessBoard.appendChild(boardSquare);
+      chessBoard.appendChild(boardSquare)
 
-      if ((i + j) % 2 === 0) boardSquare.classList.add("dark");
+      if ((i + j) % 2 === 0) boardSquare.classList.add('dark')
       if (exceptions.includes(idTracker)) {
-        if (idTracker % 10 === 0) idTracker += 1;
-        else idTracker += 2;
+        if (idTracker % 10 === 0) idTracker += 1
+        else idTracker += 2
       }
 
-      boardSquare.id = idTracker;
-      idTracker += 1;
+      boardSquare.id = idTracker
+      idTracker += 1
     }
   }
-};
+}
 
 export const isStrikeable = (choosenQueens, errorSuccessTag) => {
-  if (choosenQueens.length !== 2)
+  if (choosenQueens.length !== 2) {
     return {
       isValid: false,
-      message: "You have to pick any two queens from the board",
-    };
+      message: 'You have to pick any two queens from the board'
+    }
+  }
 
-  errorSuccessTag.innerHTML = "";
+  errorSuccessTag.innerHTML = ''
 
-  const queen1 = choosenQueens[0];
-  const queen2 = choosenQueens[1];
+  const queen1 = choosenQueens[0]
+  const queen2 = choosenQueens[1]
 
-  const cell11 = queen1.split("", queen1)[0];
-  const cell12 = queen1.split("", queen1)[1];
+  const cell11 = queen1.split('', queen1)[0]
+  const cell12 = queen1.split('', queen1)[1]
 
-  const cell21 = queen2.split("", queen2)[0];
-  const cell22 = queen2.split("", queen2)[1];
+  const cell21 = queen2.split('', queen2)[0]
+  const cell22 = queen2.split('', queen2)[1]
 
   const postitiveObj = {
     isValid: true,
-    message: "queen 1 can take down queen 2",
-  };
+    message: 'queen 1 can take down queen 2'
+  }
 
   // horizontal check
   if (cell11 === cell21) {
-    return postitiveObj;
+    return postitiveObj
   }
 
   // vertical check
   if (cell12 === cell22) {
-    return postitiveObj;
+    return postitiveObj
   }
 
   // diagonal left to right
   if (cell11 - cell12 === cell21 - cell22) {
-    return postitiveObj;
+    return postitiveObj
   }
 
-  const positionRightLeft = Math.abs(parseInt(queen1) - parseInt(queen2));
+  const positionRightLeft = Math.abs(parseInt(queen1) - parseInt(queen2))
 
-  const moduleRightLeft = positionRightLeft % 9 === 0;
+  const moduleRightLeft = positionRightLeft % 9 === 0
 
   if (moduleRightLeft) {
-    return postitiveObj;
+    return postitiveObj
   }
 
   return {
     isValid: false,
-    message: "Missed, can not attack this queen",
-  };
-};
+    message: 'Missed, can not attack this queen'
+  }
+}
